48ac38ffd05116aeffc2653957dcdba3ec03b69b
Michael Dugle <michaeldugle@Michaels-iMac.local>
2025-03-03 00:06:59 -0600

    no comment

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,153 @@
 <template>
   <h1>{{ message }}</h1>
-  <h1>Welcome, {{ formData.name }}</h1>
-  <form action="">
-    <div>
-      <span>Name:</span>
-      <span>
-        <!-- formData.name is synced after "change" event instead of "input" event. -->
-        <input type="text" v-model.lazy="formData.name" />
-      </span>
-    </div>
-    <div>
-      <span>Gender:</span>
-      <span>
-        <input type="radio" id="male" value="M" v-model="formData.gender" />
-        <label for="male">M</label>
-        <input type="radio" id="female" value="F" v-model="formData.gender" />
-        <label for="female">F</label>
-      </span>
-    </div>
-    <div>
-      <span>Age:</span>
-      <span>
-        <!-- Input is automatically typecast as a number. -->
-        <input type="text" id="age" v-model.number="formData.age" />
-      </span>
-    </div>
-    <div>
-      <span>Hobbies:</span>
-      <!-- We can bind multiple checkboxes to the same array. -->
-      <input type="checkbox" value="basketball" v-model="formData.hobbies" />
-      <label for="basketball">Basketball</label>
-      <input type="checkbox" value="football" v-model="formData.hobbies" />
-      <label for="football">Football</label>
-      <input type="checkbox" value="js" v-model="formData.hobbies" />
-      <label for="js">JavaScript</label>
-    </div>
-    <div>
-      <span>Profession:</span>
-      <select v-model="formData.profession">
-        <option value="">Please select one</option>
-        <option value="1">Software Engineer</option>
-        <option value="2">Data Scientist</option>
-        <option value="3">Product Manager</option>
-        <option value="4">Professor</option>
-      </select>
-    </div>
-    <div>
-      <span>Description:</span>
-      <!-- Leading and trailing whitespace is removed automatically. -->
-      <textarea
-        v-model.trim="formData.desc"
-        @keyup.enter="submitForm"
-      ></textarea>
-    </div>
-    <div>
-      <input type="reset" value="Reset" @click="resetForm" />
-      <!-- Prevent the form from submitting through form action. -->
-      <input type="submit" value="Submit" @click.prevent="submitForm" />
-    </div>
-  </form>
+
+  <div class="card">
+    <h1>Scenario 1: Watch a "ref(primitive value)"</h1>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by 1</button>
+  </div>
+
+  <div class="card">
+    <h1>Scenario 2: Watch a property in "ref(object)"</h1>
+    <h2>Name: {{ wizard1.name }}</h2>
+    <h2>Wand: {{ wizard1.wand }}</h2>
+    <button @click="wizard1.name = wizard1.name.toUpperCase()">
+      Change name to upper case
+    </button>
+    <button @click="changeWizard1Wand">Change wand</button>
+    <button @click="wizard1.wand.core = 'Unicorn hair'">
+      Change wand core
+    </button>
+  </div>
+
+  <div class="card">
+    <h1>Scenario 3: Watch a "ref(object)"</h1>
+    <h2>Name: {{ wizard2.name }}</h2>
+    <h2>Wand: {{ wizard2.wand }}</h2>
+    <button @click="wizard2.name = wizard2.name.toUpperCase()">
+      Change name to upper case
+    </button>
+    <button @click="wizard2.wand.core = 'Phoenix feather'">
+      Change wand core
+    </button>
+    <button @click="changeWizard">Change wizard</button>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-let message = ref('Hello, v-model!')
+let message = ref('Hello, Watchers!')
 
-let formData = ref({
-  name: '',
-  gender: '',
-  age: '',
-  hobbies: [],
-  profession: '',
-  desc: ''
+// Scenario 1: Watch a ref(primitive value), this is a common scenario.
+let number = ref(1)
+
+const stopWatch = watch(number, (newValue, oldValue) => {
+  // A callback function, called when number is changed.
+  console.log(
+    'Watch a ref(primitive value): number changes',
+    newValue,
+    oldValue
+  )
+  if (newValue >= 5) {
+    stopWatch()
+  }
 })
 
-function resetForm() {
-  formData.value = {
-    name: '',
-    gender: '',
-    age: '',
-    hobbies: [],
-    profession: '',
-    desc: ''
+// Scenario 2: Watch a property in a ref(object)
+let wizard1 = ref({
+  id: 1001,
+  name: 'Harry Potter',
+  house: 'Gryffindor',
+  age: 17,
+  wand: {
+    core: 'Phoenix feather',
+    wood: 'Holly'
+  }
+})
+
+function changeWizard1Wand() {
+  wizard1.value.wand = {
+    core: 'Dragon heartstring',
+    wood: 'Vine'
   }
 }
 
-function submitForm() {
-  console.log(JSON.stringify(formData.value))
-  // We can use axios or fetch() to submit data to the back end.
+watch(
+  () => wizard1.value.name,
+  (newValue, oldValue) => {
+    console.log(
+      'Watch a property in a ref(object): wizard1 name changes',
+      newValue,
+      oldValue
+    )
+  }
+)
+
+watch(
+  () => wizard1.value.wand,
+  (newValue, oldValue) => {
+    console.log(
+      'Watch a property in a ref(object): wizard1 wand changes',
+      newValue,
+      oldValue
+    )
+  },
+  { deep: true }
+)
+
+// Scenario 3: Watch a ref(object)
+let wizard2 = ref({
+  id: 1003,
+  name: 'Ron Weasley',
+  house: 'Gryffindor',
+  age: 17,
+  wand: {
+    core: 'Unicorn hair',
+    wood: 'Willow'
+  }
+})
+
+function changeWizard() {
+  wizard2.value = {
+    id: 1002,
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Dragon heartstring',
+      wood: 'Vine'
+    }
+  }
 }
+
+watch(
+  wizard2,
+  (newValue, oldValue) => {
+    console.log('Watch a ref(object): wizard2 changes', newValue, oldValue)
+  },
+  { deep: true }
+)
+
+// Scenario 4: Watch an array of ref(primitive value), a property of a ref(object), and a ref(object)
+watch(
+  [number, () => wizard1.value.name, wizard2],
+  (newValue, oldValue) => {
+    console.log(
+      'Watch an array of ref(primitive value), a property of a ref(object), and a ref(object)'
+    )
+  },
+  {
+    deep: true
+  }
+)
 </script>
 
 <style scoped>
-form {
-  padding: 20px;
-}
-
-form div {
-  height: 40px;
-  line-height: 40px;
-}
-
-form div span {
-  display: inline-block;
-  width: 100px;
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
 }
 </style>
