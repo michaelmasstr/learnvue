089b8d3872383a1dccdd84de5c508bfa83bada74
Michael Dugle <michaeldugle@Michaels-iMac.local>
2025-03-12 00:58:26 -0500

    Add scoped slots with StudentList component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,62 @@
 <template>
-  <BaseLayout>
-    <template #header>
-      <h1>Here might be a page title.</h1>
+  <StudentList :list="list">
+    <template #default="{ stu }">
+      <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}
+      </span>
     </template>
-    <template #main>
-      <p>A paragraph of the main content.</p>
-      <p>And another one.</p>
-    </template>
-    <template #footer>
-      <p>Here's some contact info.</p>
-    </template>
-  </BaseLayout>
+  </StudentList>
+
+  <hr />
+
+  <el-table :data="todoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status">
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup>
-import BaseLayout from './BaseLayout.vue'
+import { onMounted, ref } from 'vue'
+import StudentList from './StudentList.vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+async function getTodoList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await response.json()
+  console.log(data)
+  todoList.value = data
+}
+
+onMounted(() => {
+  getTodoList()
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.cursed {
+  color: red;
+}
+</style>
